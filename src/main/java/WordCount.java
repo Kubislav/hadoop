ef1ab3a3407fad98a345bbf302c927ebc457f24d
Kubislav <knezojakub@gmail.com>
2020-10-24 00:15:18 +0200

    zmena na 2. jobs a txtID file ako 4. argument

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -8,7 +8,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -17,109 +17,140 @@ import java.util.regex.Pattern;
 
 public class WordCount {
 
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 
         private final static IntWritable one = new IntWritable(1);
         private Text documentLine = new Text();
         private Text documentWord = new Text();
-        public List <StringTokenizer> listName = new ArrayList<>(25);
+        public List<StringTokenizer> listName = new ArrayList<>(25);
 
         public String tmp = null;
         public String[] longTmp = null;
         public String tmpID = null;
         public String longTmpID = null;
-
         public int flagMatchGetInfoBelow = 0;
+
+        String filename = "person_id.txt";
+        Writer out = new OutputStreamWriter(new FileOutputStream(filename, true), "UTF-8");
+
+        public TokenizerMapper() throws UnsupportedEncodingException, FileNotFoundException {
+        }
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
             StringTokenizer wordsFromLine = new StringTokenizer(value.toString(), "\t", false);
 
-            if (flagMatchGetInfoBelow == 1 && wordsFromLine.hasMoreTokens()){
-                try{
-                    tmp = wordsFromLine.nextToken();
-                    longTmp = tmp.split("/");
-                    tmpID = longTmp[4].substring(0, longTmp[4].length() - 1);
 
-                    if(!tmpID.equals(longTmpID)){
-                        flagMatchGetInfoBelow = 0;
-                    }else{
-                        String x = tmp;
-                        while(wordsFromLine.hasMoreTokens()){
-                            x = x + wordsFromLine.nextToken().toString();
-                        }
-                        documentWord = new Text(x);
-                        context.write(documentWord, one);
-                    }
-                }
-                catch (Exception e){
-                    System.out.println("VYPISUJEM DOLNE");
-                    System.out.println(e);
-                }
+            Pattern pattern = Pattern.compile("book.book_edition.(isbn)");
+            String riadok = value.toString();
 
-            }else
-            {
-                if(listName.size() == 25){
-                    listName.remove(0);
-                }
-                listName.add(wordsFromLine);
+            Matcher matcher = pattern.matcher(riadok);
 
-                Pattern pattern = Pattern.compile("book.book_edition.(isbn)");
-                String riadok = value.toString();
+            if (matcher.find()) {
+                try {
 
-                Matcher matcher = pattern.matcher(riadok);
 
-                if(matcher.find()){
                     String[] first = riadok.split("\t");
                     String[] second = first[0].split("/");
                     String ID = second[4].substring(0, second[4].length() - 1);
+                    documentWord = new Text(ID);
+                    out.write(ID + "\n");
+                    out.close();
+                    context.write(documentWord, one);
 
-                    try {
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
 
-                        for(int i = 0; i < listName.size(); i++){
-                            if(listName.get(i).hasMoreTokens()){
-                                tmp = listName.get(i).nextToken();
-                                longTmp = tmp.split("/");
-                                tmpID = longTmp[4].substring(0, longTmp[4].length() - 1);
+            }
 
-                                if (tmpID.equals(ID)){
-                                    String x = tmp;
-                                    while(listName.get(i).hasMoreTokens()){
-                                        x = x + listName.get(i).nextToken().toString();
-                                    }
-                                    documentWord = new Text(x);
-                                    context.write(documentWord, one);
-                                }
-                            }
+        }
+
+
+    }
+
+    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    }
+
+    public static class FindBooks extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text documentLine = new Text();
+        private Text documentWord = new Text();
+        public List<StringTokenizer> listName = new ArrayList<>(25);
+
+        String foundID = null;
+        boolean flagBookHere = false;
+        public String tmp = null;
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+            StringTokenizer wordsFromLine = new StringTokenizer(value.toString(), "\t", false);
+
+            Configuration conf = context.getConfiguration();
+            File idFile = new File(conf.get("idFile"));
+            String riadok = value.toString();
+
+            String[] first = riadok.split("\t");
+            String[] second = first[0].split("/");
+            String ID = second[4].substring(0, second[4].length() - 1);
+            ID = ID + "\t1";
+
+            if(flagBookHere && ID.equals(foundID)){
+
+                String x = wordsFromLine.nextToken();
+                while(wordsFromLine.hasMoreTokens()){
+                    x = x + wordsFromLine.nextToken();
+                }
+
+                context.write(new Text(x), one);
+            }
+            else{
+                flagBookHere = false;
+                try (BufferedReader br = new BufferedReader(new FileReader(idFile))) {
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        if(line.equals(ID)){
+                            flagBookHere = true;
+                            foundID = ID;
+                            context.write(new Text(wordsFromLine.nextToken()), one);
+                            break;
                         }
                     }
-                    catch (Exception e) {
-                        System.out.println("VYPISUJEM HORNE");
-                        System.out.println(e);
-                    }
-                    longTmpID = tmpID;
-                    flagMatchGetInfoBelow = 1;
+                }
+                catch (Exception e){
+                    System.out.println(e);
                 }
             }
-        }
 
+
+            /*if (matcher.find()) {
+                try {
+                    String[] first = riadok.split("\t");
+                    String[] second = first[0].split("/");
+                    String ID = second[4].substring(0, second[4].length() - 1);
+                    documentWord = new Text(ID);
+                    context.write(documentWord, one);
+
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+
+            }*/
+        }
 
     }
 
-    public static class printer{
-        public void printerMethod(){
-            System.out.println("tester!");
-        }
-    }
-
-    public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+    public static class BooksReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
     }
 
     public static void main(String[] args) throws Exception {
-
+        //prvy job
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "word count");
+
+        Job job = Job.getInstance(conf, "FindIDs");
         job.setJarByClass(WordCount.class);
         job.setMapperClass(TokenizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
@@ -128,7 +159,30 @@ public class WordCount {
         job.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
-        //fsefe3
+        job.waitForCompletion(true);
+        try {
+            File myTestFile = new File(args[3]);
+            System.out.println("HEY");
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        //druhy job
+        Configuration conf2 = new Configuration();
+        conf2.set("idFile", args[3]);
+
+        Job job2 = Job.getInstance(conf2, "FindBooks");
+        job2.setJarByClass(WordCount.class);
+        job2.setMapperClass(FindBooks.class);
+        job2.setCombinerClass(BooksReducer.class);
+        job2.setReducerClass(BooksReducer.class);
+        job2.setOutputKeyClass(Text.class);
+        job2.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job2, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job2, new Path(args[2]));
+        System.exit(job2.waitForCompletion(true) ? 0 : 1);
+
+
     }
 }
